b56d058bedb266d6a73721a9d2e12a30fdea4f33
Andre Preoteasa <34653577+andrepreoteasa@users.noreply.github.com>
2018-12-05 22:22:56 -0500

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,29 @@
+x = "snow"
+print()
+print (x)
+print (f"cold as {x}")
+print ("cold as {}".format(x))
+
+a = "A"
+b = "n"
+c = "d"
+d = "r"
+e = "e"
+
+print (a,b,c,d,e)
+print (a + b + c + d + e)
+print()
+
+#this creates a string called this_string
+this_string = "{} {}"
+
+#strings have a function called format with and I've set it up with two arguments
+print(this_string.format("one", "two"))
+
+print()
+print()
+print("""
+I am the best
+\"programmer\"
+in the world.  Look, I made back slash \\
+""")
